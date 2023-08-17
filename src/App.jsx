ae8e9dad2992ec8441b7e8ca3cca665c647f05bd
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2023-08-17 16:54:47 +0200

    added more params to search and useState to manage them

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
 
 const App = () => {
-	return (
-		<div>
-			<h1>Adopt Me!</h1>
-			<SearchParams />
-		</div>
-	)
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
